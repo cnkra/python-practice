f251b4b382c6e6d90f0b0e037983e2f74aec6ec2
cnkra <canhuseyinkara@gmail.com>
2025-04-01 01:18:13 +0200

    Added data types exercises

---
+++ sum.py
@@ -0,0 +1,5 @@
+Python 3.13.2 (main, Feb  4 2025, 14:51:09) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = 1
+>>> num2 = 2
+>>> sum = num1 + num2
